ed90703621816a5539d8129587c7caf9ad876fa9
hjespersen <hjj@bang-olufsen.dk>
2018-11-16 11:47:08 +0100

    added tools and plugins for vscode

---
+++ install-tools.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y doxygen clang-format-4.0 python-pip graphviz pv
+pip install pycrypto
+pip install cpplint
--- install-vscode.sh
+++ install-vscode.sh
@@ -5,3 +5,11 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 sudo apt install -y apt-transport-https
 sudo apt update
 sudo apt install -y code
+
+# install extensions
+code --install-extension ms-vscode.cpptools
+code --install-extension twxs.cmake
+code --install-extension xaver.clang-format
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension mine.cpplint
+code --install-extension cschlosser.doxdocgen
