c78a37ad86ab8d8196aea034614225f406d1161f
hjespersen <hjj@bang-olufsen.dk>
2018-11-16 12:27:02 +0100

    added virtualbox guest additions

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -7,7 +7,9 @@ sudo apt update
 sudo apt -y install gcc make linux-headers-$(uname -r) dkms
 sudo apt update
 sudo apt install -y virtualbox-5.2
+# extension pack
 wget https://download.virtualbox.org/virtualbox/5.2.22/Oracle_VM_VirtualBox_Extension_Pack-5.2.22.vbox-extpack
 vboxmanage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-5.2.22.vbox-extpack
+# guest additions
+wget http://download.virtualbox.org/virtualbox/5.2.22/VBoxGuestAdditions_5.2.22.iso -P /home/$USER/
 sudo usermod -aG vboxusers $USER
-
